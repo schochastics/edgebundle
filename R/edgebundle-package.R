fc571e7d1ef8351ca3e4893084ba55df6a2a9e94
schochastics <david@schochastics.net>
2022-11-21 10:07:28 +0100

    fixed whitespace

--- R/edgebundle-package.R
+++ R/edgebundle-package.R
@@ -5,12 +5,12 @@
 #' An R package that implements several edge bundling/flow and metro map algorithms.
 #' It includes:
 #'
-#' Force directed edge bundling `edge_bundle_force()`
-#' Stub bundling  `edge_bundle_stub()`
-#' Hammer bundling  `edge_bundle_hammer()`
-#' Edge-path bundling  `edge_bundle_path()`
-#' TNSS flow map `tnss_*()`
-#' Multicriteria Metro map layout `metro_multicriteria()`
+#' - Force directed edge bundling `edge_bundle_force()`
+#' - Stub bundling  `edge_bundle_stub()`
+#' - Hammer bundling  `edge_bundle_hammer()`
+#' - Edge-path bundling  `edge_bundle_path()`
+#' - TNSS flow map `tnss_*()`
+#' - Multicriteria Metro map layout `metro_multicriteria()`
 #'
 #' @keywords internal
 "_PACKAGE"
